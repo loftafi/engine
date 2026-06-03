84b6b219088fbc9b74fa88cd482936228118eb3d
loftafi <loftafi@proton.me>
2026-06-03 11:28:07 +1000

    Add Zig license. (Zig std is a dependency)

--- src/License.zig
+++ src/License.zig
@@ -14,9 +14,9 @@ contents: []const u8,
 /// Full text of relevant license
 license: []const u8,
 
-pub const licenses: []const License = &.{
+pub const licenses = &[_]License{
     .{
-        .library = "sdl",
+        .library = "SDL3",
         .copyright = "Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>",
         .contents = "zlib",
         .license =
@@ -40,7 +40,7 @@ pub const licenses: []const License = &.{
         ,
     },
     .{
-        .library = "sdl mixer",
+        .library = "SDL Mixer",
         .copyright = "Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>",
         .contents = "zlib",
         .license =
@@ -430,6 +430,34 @@ pub const licenses: []const License = &.{
         \\DAMAGES, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
         \\FROM, OUT OF THE USE OR INABILITY TO USE THE FONT SOFTWARE OR FROM
         \\OTHER DEALINGS IN THE FONT SOFTWARE.
+        ,
+    },
+    .{
+        .library = "zig",
+        .copyright = "Copyright (c) Zig contributors",
+        .contents = "MIT",
+        .license =
+        \\The MIT License (Expat)
+        \\
+        \\Copyright (c) Zig contributors
+        \\
+        \\Permission is hereby granted, free of charge, to any person obtaining a copy
+        \\of this software and associated documentation files (the "Software"), to deal
+        \\in the Software without restriction, including without limitation the rights
+        \\to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        \\copies of the Software, and to permit persons to whom the Software is
+        \\furnished to do so, subject to the following conditions:
+        \\
+        \\The above copyright notice and this permission notice shall be included in
+        \\all copies or substantial portions of the Software.
+        \\
+        \\THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        \\IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        \\FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        \\AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        \\LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        \\OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+        \\THE SOFTWARE.
         ,
     },
 };
