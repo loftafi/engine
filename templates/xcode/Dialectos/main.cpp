4b41adeabe23d291b810a92c62dba18d06cbac19
loftafi <loftafi@proton.me>
2026-09-02 11:56:56 +1000

    Add basic xcode template

---
+++ templates/xcode/Dialectos/main.cpp
@@ -0,0 +1,10 @@
+//
+// Placeholder cpp file to declare SDL_MAIN_NEEDED
+//
+//#include "main.hpp"
+//#include <SDL3/SDL_main.h>
+//#include <SDL3/SDL.h>
+//#include <SDL3/SDL_render.h>
+//#define SDL_MAIN_NEEDED
+//#define SDL_MAIN_HANDLED
+//#define SDL_MAIN_USE_CALLBACKS
