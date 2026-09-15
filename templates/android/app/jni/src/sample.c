4dfb830b8186aaface52068be5b7c5447174dc2a
loftafi <loftafi@proton.me>
2026-09-16 09:14:46 +1000

    Add SDL to android project template

--- templates/android/app/jni/src/sample.c
+++ templates/android/app/jni/src/sample.c
@@ -3,31 +3,29 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#if 0
 extern uint AppInitC(int argc, char *argv[]);
 extern void AppQuitC(SDL_AppResult result);
 extern uint AppEventC(SDL_Event *event);
 extern uint AppIterateC(void *appstate);
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
+SDL_AppResult SDL_AppIterate(void *appstate) {
     // Call the shared library to handle events.
-    return AppIterateC(appstate);
+   return AppIterateC(appstate);
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     // Call the shared library to handle events.
     return AppEventC(event);
 }
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     // Call the shared library to handle setup.
     return AppInitC(argc, argv);
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     // Call the shared library to hande cleanup.
     AppQuitC(result);
 }
+#endif
